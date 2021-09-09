1abfb52f7738630614a41bea8ff21dcfd6df7edc
CaiShuangLian <2243157861@qq.com>
2021-09-09 19:57:07 +0800

    UserController

---
+++ src/main/java/com/csl/seckill/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.csl.seckill.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author CaiShuangLian
+ * @since 2021-09-09
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
