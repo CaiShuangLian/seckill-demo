cff4735436ac3d720ecaf90989bcc8664b02dc5a
CaiShuangLian <2243157861@qq.com>
2021-09-14 10:23:54 +0800

    登录功能

--- src/main/java/com/csl/seckill/service/impl/UserServiceImpl.java
+++ src/main/java/com/csl/seckill/service/impl/UserServiceImpl.java
@@ -1,17 +1,21 @@
 package com.csl.seckill.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.csl.seckill.exception.GlobalException;
 import com.csl.seckill.mapper.UserMapper;
 import com.csl.seckill.pojo.User;
 import com.csl.seckill.service.IUserService;
+import com.csl.seckill.utils.CookieUtil;
 import com.csl.seckill.utils.MD5Util;
-import com.csl.seckill.utils.ValidatorUtil;
+import com.csl.seckill.utils.UUIDUtil;
 import com.csl.seckill.vo.LoginVo;
 import com.csl.seckill.vo.RespBean;
 import com.csl.seckill.vo.RespBeanEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -30,29 +34,39 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     /**
      * 登录功能
      * @param loginVo
+     * @param request
+     * @param response
      * @return
      */
     @Override
-    public RespBean doLogin(LoginVo loginVo) {
+    public RespBean doLogin(LoginVo loginVo, HttpServletRequest request, HttpServletResponse response) {
         String mobile = loginVo.getMobile();
         String password = loginVo.getPassword();
-        //判断用户名或者密码是否为空
-        if(StringUtils.isEmpty(mobile)||StringUtils.isEmpty(password)){
-            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
-        }
-        //判断手机号输入是否正确
-        if(!ValidatorUtil.isMobile(mobile)){
-            return RespBean.error(RespBeanEnum.MOBILE_ERROR);
-        }
+//        //判断用户名或者密码是否为空
+//        if(StringUtils.isEmpty(mobile)||StringUtils.isEmpty(password)){
+//            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
+//        }
+//        //判断手机号输入是否正确
+//        if(!ValidatorUtil.isMobile(mobile)){
+//            return RespBean.error(RespBeanEnum.MOBILE_ERROR);
+//        }
         //根据手机号获取用户
         User user = userMapper.selectById(mobile);
-        if(user==null)
-            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
-
+        if(user==null){
+//            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
+            throw new GlobalException(RespBeanEnum.LOGIN_ERROR);
+        }
         //判断密码是否正确
         if(!MD5Util.formPassToDBPass(password,user.getSalt()).equals(user.getPwd())){
-            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
+//            return RespBean.error(RespBeanEnum.LOGIN_ERROR);
+            throw new GlobalException(RespBeanEnum.LOGIN_ERROR);
         }
+        //生成cookie
+        String ticket= UUIDUtil.uuid();
+        //获取浏览器的session
+        request.getSession().setAttribute(ticket,user);
+        //将session存储在cookie中
+        CookieUtil.setCookie(request,response,"userTicket",ticket);
         return RespBean.success();
     }
 }
